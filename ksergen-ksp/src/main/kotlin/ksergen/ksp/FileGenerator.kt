aa040589c95c11221a69259295f6949e33b5e11a
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-19 21:43:01 +0200

    feat(ksp): generate constructor for data class in generateImmutableFile()

--- ksergen-ksp/src/main/kotlin/ksergen/ksp/FileGenerator.kt
+++ ksergen-ksp/src/main/kotlin/ksergen/ksp/FileGenerator.kt
@@ -2,6 +2,11 @@ package ksergen.ksp
 
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.ksp.toTypeName
 
 fun generateImmutableFile(
     packageName: String,
@@ -12,6 +17,31 @@ fun generateImmutableFile(
         packageName = packageName,
         fileName = fileName,
     ).apply {
-
+        declarationList.forEach { declaration ->
+            addType(
+                TypeSpec.classBuilder(declaration.simpleName.getShortName().drop(7))
+                    .addModifiers(KModifier.DATA)
+                    .primaryConstructor(
+                        FunSpec.constructorBuilder().apply {
+                            declaration.primaryConstructor!!.parameters.forEach { parameter ->
+                               addParameter(
+                                   name = parameter.name!!.getShortName(),
+                                   type = parameter.type.toTypeName()
+                               )
+                            }
+                        }.build()
+                    ).apply {
+                        declaration.primaryConstructor!!.parameters.forEach { parameter ->
+                            addProperty(
+                                PropertySpec.builder(
+                                    parameter.name!!.getShortName(),
+                                    parameter.type.toTypeName()
+                                ).initializer(parameter.name!!.getShortName()).build()
+                            )
+                        }
+                    }
+                    .build()
+            )
+        }
     }.build()
 }
