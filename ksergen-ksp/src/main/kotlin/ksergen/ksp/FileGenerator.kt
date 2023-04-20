f05f6ca244c5e146602a7bd00313667c9ccb9280
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-20 18:15:10 +0200

    style: remove unnecessary new line

--- ksergen-ksp/src/main/kotlin/ksergen/ksp/FileGenerator.kt
+++ ksergen-ksp/src/main/kotlin/ksergen/ksp/FileGenerator.kt
@@ -42,8 +42,7 @@ fun generateImmutableFile(
                                 ).initializer(parameter.name!!.getShortName()).build()
                             )
                         }
-                    }
-                    .build()
+                    }.build()
             )
         }
     }.build()
