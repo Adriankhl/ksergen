d3cf748434fec2f4cc91aa51178b38d7d66654ea
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-22 01:38:12 +0200

    test: add MutablePolymorphicData()

--- ksergen-test/src/main/kotlin/ksergen/mock/PolymorphicData.kt
+++ ksergen-test/src/main/kotlin/ksergen/mock/PolymorphicData.kt
@@ -10,3 +10,12 @@ data class MutableSimpleMasterData(val s: Int = 9): MutablePolymorphicSealData()
 
 @GenerateImmutable
 sealed interface MutableDummyInterface
+
+@GenerateImmutable
+data class MutablePolymorphicData(
+    val d: MutablePolymorphicSealData = MutableMasterData(),
+    val dm: MutableMap<Int, MutablePolymorphicSealData> = mutableMapOf(
+        1 to MutableMasterData(),
+        2 to MutableSimpleMasterData(),
+    )
+)
