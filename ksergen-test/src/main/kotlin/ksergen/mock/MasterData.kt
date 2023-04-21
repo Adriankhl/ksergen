2352d15d474f96abd51e40e44c092ff5ecedb221
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-21 15:56:24 +0200

    test: add MutableMasterDataTest

--- ksergen-test/src/main/kotlin/ksergen/mock/MasterData.kt
+++ ksergen-test/src/main/kotlin/ksergen/mock/MasterData.kt
@@ -6,17 +6,30 @@ import ksergen.mock.base.MutableIntData
 
 @GenerateImmutable
 data class MutableMasterData(
-    var dataName: String,
-    var id: MutableIntData,
-    var td: MutableDoubleData,
-    val intList: MutableList<MutableIntData>,
-    val doubleSet: MutableSet<MutableDoubleData>,
-    var doubleMap: MutableMap<Int, MutableDoubleData>,
-    val nestedMap: MutableMap<Int, MutableMap<Int, MutableDoubleData>>,
-    val weirdCollection: MutableMap<MutableIntData, MutableMap<Int, MutableMap<String, MutableDoubleData>>>,
+    var dataName: String = "Hello",
+    var id: MutableIntData = MutableIntData(1, 2),
+    var td: MutableDoubleData = MutableDoubleData(1.3, 6.7),
+    val intList: MutableList<MutableIntData> = mutableListOf(
+        MutableIntData(15, 24),
+        MutableIntData(-1, -4)
+    ),
+    val doubleSet: MutableSet<MutableDoubleData> = mutableSetOf(
+        MutableDoubleData(0.2, 0.4),
+        MutableDoubleData(9.5, 123.5)
+    ),
+    var doubleMap: MutableMap<Int, MutableDoubleData> = mutableMapOf(
+        4 to MutableDoubleData(3.4, 5.6),
+        900 to MutableDoubleData(3.5, 7.8)
+    ),
+    val nestedMap: MutableMap<Int, MutableMap<Int, MutableDoubleData>> = mutableMapOf(
+        1 to mutableMapOf(
+            345 to MutableDoubleData(2.3, 40.0),
+            98 to MutableDoubleData(345.2, 45891.2)
+        ),
+        8 to mutableMapOf(
+            34235 to MutableDoubleData(22342.3, 423420.0),
+            92348 to MutableDoubleData(344565.2, 4582391.2)
+        )
+    ),
+    val emptyMap: MutableMap<Int, MutableIntData> = mutableMapOf(),
 )
-
-//val intList: MutableList<MutableIntData>,
-//val doubleSet: MutableSet<MutableDoubleData>,
-//var doubleMap: MutableMap<Int, MutableDoubleData>,
-//val weirdCollection: MutableMap<MutableIntData, MutableMap<Int, MutableMap<String, MutableDoubleData>>>,
---
+++ ksergen-test/src/test/kotlin/ksergen/mock/MutableMasterDataTest.kt
@@ -0,0 +1,24 @@
+package ksergen.mock
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MutableMasterDataTest {
+    @Test
+    fun serializationTest() {
+        val format = Json {
+            encodeDefaults = true
+        }
+
+        val a = MutableMasterData()
+        val b: String = format.encodeToString(a)
+        val c: MasterData = format.decodeFromString(b)
+        val d: String = format.encodeToString(c)
+        val e: MutableMasterData = format.decodeFromString(d)
+
+        assertEquals(a, e)
+    }
+}
