0f341af73d23b8f91d4344242d80441434720a69
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-22 02:11:02 +0200

    test: add assertion in MutablePolymorphicDataTest

--- ksergen-test/src/test/kotlin/ksergen/mock/MutablePolymorphicDataTest.kt
+++ ksergen-test/src/test/kotlin/ksergen/mock/MutablePolymorphicDataTest.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 internal class MutablePolymorphicDataTest {
     @Test
@@ -15,5 +16,9 @@ internal class MutablePolymorphicDataTest {
         val a = MutablePolymorphicData()
         val b: String = format.encodeToString(a)
         val c: PolymorphicData = format.decodeFromString(b)
+        val d: String = format.encodeToString(c)
+        val e: MutablePolymorphicData = format.decodeFromString(d)
+
+        assertEquals(a, e)
     }
 }
