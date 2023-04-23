12911708a1588e7d03f711d4d6f3184a0acdefc1
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-24 01:22:09 +0200

    docs: improve GenerateImmutable documentation

--- ksergen-annotations/src/main/kotlin/ksergen/annotations/GenerateImmutable.kt
+++ ksergen-annotations/src/main/kotlin/ksergen/annotations/GenerateImmutable.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.MetaSerializable
 /**
  * Generate a serializable immutable copy of this class.
  * This class is also automatically serializable.
+ * Can't set Retention to source since it breaks serialization
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.CLASS)
