a59ade2fc51a23a08fb69dbbbc1ccc212d059463
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-08-10 11:38:31 +0200

    build: filter out non-stable update suggestions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.benmanes.gradle.versions.reporter.PlainTextReporter
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
@@ -11,5 +14,43 @@ allprojects {
         mavenLocal()
         mavenCentral()
         google()
+    }
+}
+
+
+tasks.withType<DependencyUpdatesTask> {
+    gradleReleaseChannel = "current"
+
+    val showUnresolved: Boolean = project.hasProperty("showUnresolved")
+
+    fun isNonStable(version: String): Boolean {
+        return listOf(
+            "-alpha",
+            "-beta",
+            "-dev",
+            "-rc",
+        ).any {
+            version.lowercase().contains(it)
+        }
+    }
+
+    outputFormatter {
+        if (!showUnresolved) {
+            unresolved.dependencies.clear()
+        }
+
+        // temporary fix for: https://github.com/ben-manes/gradle-versions-plugin/issues/733
+        outdated.dependencies.removeAll { isNonStable(it.available.milestone.orEmpty()) }
+
+        val plainTextReporter = PlainTextReporter(
+            project,
+            revision,
+            gradleReleaseChannel
+        )
+        plainTextReporter.write(System.out, this)
+    }
+
+    rejectVersionIf {
+        isNonStable(candidate.version)
     }
 }
